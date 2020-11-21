fa20f754b7aae59602a27e340fdae576b01e5771
dika <dikalaksana>
2020-11-22 06:21:18 +0800

    Adding bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+var path = require('path');
 
 const app = express();
 
+
+app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
+
 app.get('/', (req, res) => {
-    res.send("Its Workingg!!!")
+    res.render('coba.ejs', {
+        title: "Jinx",
+    });   
 })
 
 const port = process.env.port || 3000;
