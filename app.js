e009420107c2bb8e7e710ce4aab6a60cef90c750
dika <dikalaksana>
2020-11-22 04:31:35 +0800

    change port

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.send("Its Workingg!!!")
 })
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
     console.log('itsworking')
 });
