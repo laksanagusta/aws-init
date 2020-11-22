046c39839f94971a2c56549a25c6f1cebb795283
dika <dikalaksana>
2020-11-22 12:27:45 +0800

    Change Apps

--- app.js
+++ app.js
@@ -52,6 +52,23 @@ app.use(bodyParser.json());
 // enabling CORS for all requests
 app.use(cors());
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 600000 }
+}));
+app.use(flash());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 
@@ -66,3 +83,33 @@ const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log('itsworking')
 });
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//admin
+app.use('/admin', adminRouter);
+app.use('/category', categoryRouter);
+app.use('/bank', bankRouter);
+app.use('/product', productRouter);
+app.use('/feature', featureRouter);
+app.use('/api/v1', apiRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
